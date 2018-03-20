8b3c4e3a99ba172d86e8952fe8baeef3263a778b
Charles Lane <charleslane23@gmail.com>
2018-03-20 10:14:46 -0400

    deploy

--- bonds/settings/settings-dev.py
+++ bonds/settings/settings-dev.py
@@ -174,7 +174,6 @@ MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-print (STATIC_URL, 'URL')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
