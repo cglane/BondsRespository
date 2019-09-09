1d0a6685f9cb095aac0fde10bc0ef0671872dab5
Charles Lane <charleslane23@gmail.com>
2019-09-09 10:58:12 -0400

    fix inventory

--- powers/custom_admin.py
+++ powers/custom_admin.py
@@ -13,9 +13,6 @@ class CustomAdminSite(AdminSite):
 	# extend / override admin views, such as `index()`
 	@never_cache
 	def index(self, request, extra_context=None):
-		print('here')
-		extra_context = extra_context or {}
-
 		# do whatever you want to do and save the values in `extra_context`
 		extra_context = {
 			'all_agents': agent_powers_count(),
