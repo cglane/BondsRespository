89a8a4a7d13d6043ed0a8083ad6fc221232e5e89
Charles Lane <charleslane23@gmail.com>
2020-06-18 16:03:28 -0400

    remove dotenv

--- bonds/settings/settings-dev.py
+++ bonds/settings/settings-dev.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import sys
 from dotenv import load_dotenv, find_dotenv
-load_dotenv()
+# load_dotenv()
 
 def location(x):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
