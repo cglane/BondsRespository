36ad2389f6aef8d85aa6191ec661a95b5f5edb67
Charles Lane <charleslane23@gmail.com>
2020-06-18 16:17:28 -0400

    try again

--- bonds/settings/settings-dev.py
+++ bonds/settings/settings-dev.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import sys
 from dotenv import load_dotenv, find_dotenv
-# load_dotenv()
+if os.environ.get('ENVIRONMENT_NAME') != 'development':
+    load_dotenv()
 
 def location(x):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
